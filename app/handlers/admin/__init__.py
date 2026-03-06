6424530ee41fcf162a60a58e02aaa43c7c485198
admin_eas <a.evgl@yandex.ru>
2026-03-07 01:41:35 +0500

    Изменения 03.3 Устранение ошибки.

--- app/handlers/admin/__init__.py
+++ app/handlers/admin/__init__.py
@@ -1,18 +1,6 @@
 """
-Пакет обработчиков административной части
-===========================================
-Объединяет роутеры админки и предоставляет единый роутер для подключения.
+Admin handlers package
 """
+from .admin import router as combined_router
 
-from maxapi import Router
-
-from .admin import router as admin_router
-# from .api_settings import router as api_settings_router  # временно отключено (Local API не актуален)
-
-# Создаём комбинированный роутер для административных функций
-combined_router = Router()
-combined_router.include_router(admin_router)
-# combined_router.include_router(api_settings_router)  # закомментировано
-
-# Экспортируем комбинированный роутер для подключения в handlers/__init__.py
 __all__ = ["combined_router"]
